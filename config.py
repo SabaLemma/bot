acd2d69bbf1d38ef43ea5daa9fd02749f6224fda
saba lemma <sabalemma@sabas-MacBook-Air.local>
2018-04-03 21:46:21 -0400

    commit

---
+++ config.py
@@ -0,0 +1,6 @@
+# These keys are not valid. Use your own API keys
+
+consumer_key='7Pe8j6ayNyx0aCIFRZCKjfFZ2'
+consumer_secret='LJtWZJtWsgb54ogSXIwgnEd8kHAdzYZvneRclbM8dGTujXuVrG'
+access_token='2479773992-WyHyGnDVdzgg1wGByRi5XOBZmxmFzoHJHXito27'
+access_token_Secret='PtLYC4V5eqgPC18rSSN0QsqG6wTesFOnrSLSqiiO1t6aJ'
